7b6931386a4bf8de462292959e4592ec0043b216
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2023-01-29 19:29:16 +0900

    #1 - Spring Data REST 테스트 제외 처리
    프로젝트의 비즈니스 로직으로 구현한 내용이 아닌
    data rest 기능이며 통합테스트로 db에 영향을 주므로
    제외 처리해서 전체 테스트 중에는 영향이 가지 않도록 함

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외")
 @DisplayName("Data REST API 테스트")
 // 통합 테스트 -> repository까지 실행 -> DB 접근, Hibernate query까지
 @Transactional // 각 테스트마다 롤백
